91c8f7ed50147b9af01ce56b4f675c5c52f83ec4
AdolfoMoreno-08 <adolfomoren8@gmail.com>
2026-06-07 18:09:17 -0500

    Update FooterWrapper.tsx

--- components/blocks/FooterWrapper.tsx
+++ components/blocks/FooterWrapper.tsx
@@ -1,21 +1,6 @@
 "use client";
-
-import { useState } from "react";
 import Footer from "./Footer";
-import ContactForm from "./ContactForm";
 
 export default function FooterWrapper() {
-    const [isContactOpen, setIsContactOpen] = useState(false);
-
-    return (
-        <>
-            <Footer onContactClick={() => setIsContactOpen(true)} />
-
-            {/* Tu modal de contacto */}
-            <ContactForm
-                isOpen={isContactOpen}
-                onClose={() => setIsContactOpen(false)}
-            />
-        </>
-    );
+    return <Footer />;
 }
